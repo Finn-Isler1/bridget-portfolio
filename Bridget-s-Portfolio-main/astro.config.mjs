0e99137be57e94c16b54fc42dc111146696fe98f
Finn_Isler <islerfinn@gmail.com>
2026-03-13 17:11:00 -0400

    Update workflow to build from root, fix Header active class for subpath

--- Bridget-s-Portfolio-main/astro.config.mjs
+++ Bridget-s-Portfolio-main/astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://finn-isler1.github.io/bridget-portfolio",
   base: "/bridget-portfolio",
 });
